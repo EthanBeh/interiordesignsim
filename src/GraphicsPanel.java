833c5870713990bb871f25e16af963f95039ff37
BT_1N3_08 <sample@bths.edu>
2024-05-15 13:00:32 -0400

    ummm

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class GraphicsPanel {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GraphicsPanel extends JPanel implements KeyListener, MouseListener {
+    private BufferedImage astral;
+    private BufferedImage kitchen;
+    private BufferedImage bathroom;
+    private BufferedImage bedroom;
+
+    public GraphicsPanel() {
+        try {
+            astral = ImageIO.read(new File("src/astral.jpg"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            kitchen = ImageIO.read(new File("src/kitchen.jpg"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            bathroom = ImageIO.read(new File("src/bathroom.jpg"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            bedroom = ImageIO.read(new File("src/bedroom.jpg"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(astral, 0, 0, null);
+        g.setFont(new Font("Courier New", Font.BOLD, 24));
+        g.drawString("WELCOME TO INTERIOR DESIGN SIM", 480, 290);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame implements Runnable {
+    private GraphicsPanel p;
+
+    public MainFrame() {
+        JFrame f = new JFrame("Interior Design on JOHN");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(960, 580); // 540 height of image + 40 for window menu bar
+        f.setLocationRelativeTo(null); // auto-centers frame in screen
+
+        // create and add panel
+        p = new GraphicsPanel();
+        f.add(p);
+
+        // display the frame
+        f.setVisible(true);
+
+        // start thread, required for animation
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    public void run() {
+        while (true) {
+            p.repaint();  // we don't ever call "paintComponent" directly, but call this to refresh the panel
+        }
+    }
 }
