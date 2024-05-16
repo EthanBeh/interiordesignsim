d9e1ffc0faac4bcf1c98f1b31bf8a038065ef230
BT_1N3_08 <sample@bths.edu>
2024-05-16 08:30:33 -0400

    CENTERED TEXT !!! IMAGE IS CORRECT !!!!

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -39,8 +39,13 @@ public class GraphicsPanel extends JPanel implements KeyListener, MouseListener 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(astral, 0, 0, null);
-        g.setFont(new Font("Courier New", Font.BOLD, 24));
-        g.drawString("WELCOME TO INTERIOR DESIGN SIM", 480, 290);
+        //code adapted from https://stackoverflow.com/a/27740330
+        Font f = new Font("Courier New", Font.BOLD, 24);
+        g.setFont(f);
+        g.setColor(Color.WHITE);
+        String toPrint = "WELCOME TO INTERIOR DESIGN SIM";
+        FontMetrics metrics = g.getFontMetrics(f);
+        g.drawString(toPrint, (960 - metrics.stringWidth(toPrint)) / 2, (540 - metrics.getHeight()) / 2 + metrics.getAscent());
     }
     @Override
     public void keyTyped(KeyEvent e) {
